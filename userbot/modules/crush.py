6fb4234b0369a2d86776b6f844b82f71220e5450
rynkings <ryanaldi383@gmail.com>
2021-02-06 13:53:02 +0700

    Added crush file for my crush to access mybot.

---
+++ userbot/modules/crush.py
@@ -0,0 +1,13 @@
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, PRINT_LOGS
+from userbot.events import register
+
+CRUSH_ID = 1391726819
+
+@register(incoming=True, ignore_unsafe=True, disable_errors=True)
+def only_my_crush(message):
+	if str(message.text).lower().startswith('.') and message.peer_id.user_id == CRUSH_ID:
+		await message.delete()
+		await message.reply(message.text)
+
+		if BOTLOG:
+			await PRINT_LOGS(f"You're crush use `{message.text}` command")
