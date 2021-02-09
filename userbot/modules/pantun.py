bb84908bdde858e7e0268c7892f79cb9c978df70
rynkings <ryanaldi383@gmail.com>
2021-02-09 07:39:54 +0700

    Added pantun

---
+++ userbot/modules/pantun.py
@@ -0,0 +1,25 @@
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, PRINT_LOGS
+from userbot.events import register
+
+import requests
+import asyncio
+
+@register(outgoing=True, pattern=r'^\.pantun$')
+async def pantun(message):
+	request = requests.get('https://hujanapi.herokuapp.com/api/pantun?apikey=yahaha')
+	try:
+		data = request.json()
+	except Exception as e:
+		await PRINT_LOGS(str(e))
+		await message.edit('`Sorry there is some error I found.`')
+		await asyncio.sleep(5)
+		await message.delete()
+		return
+
+	if data.status_code == 200:
+		m1 = await message.edit(data['result']['result'])
+		m2 = await message.reply('Sorry the message will be deleted for 20 seconds')
+		await asyncio.sleep(3)
+		await m2.delete()
+		await asyncio.sleep(17)
+		await m1.delete()
